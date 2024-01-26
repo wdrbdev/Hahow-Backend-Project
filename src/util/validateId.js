eed87f3c410ae6386c24017da28baff3b4613d54
dev <dev@mbp.local>
2024-01-26 15:05:59 +0800

    feat: make 0 as invalid ID

--- src/util/validateId.js
+++ src/util/validateId.js
@@ -2,7 +2,7 @@ const InvalidParamError = require('../error/InvalidParamError')
 
 const numRegex = /^\d+$/
 module.exports = function validateId(idStr) {
-  if (!idStr || typeof idStr !== 'string' || !numRegex.test(idStr)) {
+  if (!idStr || typeof idStr !== 'string' || !numRegex.test(idStr) || parseInt(idStr) == 0) {
     throw new InvalidParamError(`id: '${idStr}'`)
   }
 }
